9e68bf94d1c2dfd26086908f07054a71813a296d
Paulo Marinas <paulopmx@gmail.com>
2019-09-20 17:31:52 +0800

    improved report format, ability to delete unused items

--- i18n-generate-json.js
+++ i18n-generate-json.js
@@ -1,39 +1,36 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const glob = require('glob');
-const _ = require('lodash/fp');
-const {
-  transformise
-} = require('./index');
+const fs = require('fs')
+const glob = require('glob')
+const _ = require('lodash/fp')
+const { transformise } = require('./index')
 
 class i18nGenerateJson {
   constructor(options) {
-    this.options = _.extend({
-      base: '',
-      from: [],
-      to: '',
-      languages: ['en'],
-      sourceLanguage: 'zh-CN',
-      autoTranslate: false,
-      extensions: ['vue', 'js'],
-      functionName: '\\$t',
-      willTransformise: false
-    }, options);
+    this.options = _.extend(
+      {
+        base: '',
+        from: [],
+        to: '',
+        languages: ['en'],
+        sourceLanguage: 'zh-CN',
+        autoTranslate: false,
+        extensions: ['vue', 'js'],
+        functionName: '\\$t',
+        willTransformise: false
+      },
+      options
+    )
   }
 
   run() {
-    const {
-      from,
-      extensions,
-      base
-    } = this.options
-    let path = from.join('|');
-    let ext = extensions.join('|');
+    const { from, extensions, base } = this.options
+    let path = from.join('|')
+    let ext = extensions.join('|')
     glob(`${base}/@(${path})/**/*.@(${ext})`, {}, (err, files) => {
-      if (err) throw err;
-      this.writeJSON(this.getText(files));
-    });
+      if (err) throw err
+      this.writeJSON(this.getText(files))
+    })
   }
 
   getText(files) {
@@ -41,138 +38,157 @@ class i18nGenerateJson {
       _.compact,
       _.uniq,
       _.flatten,
-      _.map((file) => {
-        const text = fs.readFileSync(file, 'utf8');
-        const findTranslations = new RegExp(`\\W${this.options.functionName}\\(\\'([^\\']*)\\'(\\)|,)`, "g");
-        let result;
-        let array = [];
-        while (result = findTranslations.exec(text)) {
-          array.push(result[1]);
+      _.map(file => {
+        const text = fs.readFileSync(file, 'utf8')
+        const findTranslations = new RegExp(
+          `\\W${this.options.functionName}\\(\\'([^\\']*)\\'(\\)|,)`,
+          'g'
+        )
+        let result
+        let array = []
+        while ((result = findTranslations.exec(text))) {
+          array.push(result[1])
         }
-        return array;
+        return array
       })
-    )(files);
+    )(files)
   }
 
   getLocaleConfig(language) {
     try {
-      const {
-        base,
-        to
-      } = this.options;
-      const content = fs.readFileSync(`${base}/${to}/${language}.json`);
-      return JSON.parse(content);
+      const { base, to } = this.options
+      const content = fs.readFileSync(`${base}/${to}/${language}.json`)
+      return JSON.parse(content)
     } catch (error) {
-      console.warn(`No translation file exists for language "${language}"`);
+      console.warn(`No translation file exists for language "${language}"`)
     }
-    return {};
+    return {}
   }
 
   writeJSON(value) {
-    let timer, lock;
-    let index = 0;
+    let timer, lock
+    let index = 0
     const {
       languages,
       base,
       to,
       willTransformise,
-      autoTranslate
-    } = this.options;
+      autoTranslate,
+      deleteExpired
+    } = this.options
     timer = setInterval(() => {
       if (lock) {
-        return; //第一个还没执行完则返回
+        return //第一个还没执行完则返回
       }
       if (languages.length <= index) {
-        clearInterval(timer);
-        return;
+        clearInterval(timer)
+        return
       }
-      lock = true;
-      let language = languages[index++];
-      const localeText = this.getLocaleConfig(language);
-      const foundMap = _.keyBy((str) => {
-        return willTransformise ? transformise(str) : str;
-      })(value);
+      lock = true
+      let language = languages[index++]
+      let localeText = this.getLocaleConfig(language)
+
+      const foundMap = _.keyBy(str => {
+        return willTransformise ? transformise(str) : str
+      })(value)
+
       const newTranslations = _.pickBy((v, key) => {
-        const found = localeText[key];
-        return !found;
-      })(foundMap);
+        const found = localeText[key]
+        return !found
+      })(foundMap)
 
       console.log(`\n${language}: ${base}/${to}/${language}.json`)
 
-      let newTranslationsKeys = _.keys(newTranslations)
-      console.log(`${newTranslationsKeys.length} new item(s) added`);
-      if (newTranslationsKeys.length && autoTranslate) {
-        console.table(newTranslationsKeys)
+      let report = {}
+
+      for (let i in localeText) {
+        if (value.indexOf(i) < 0) {
+          if (deleteExpired) delete localeText[i]
+          else report[i] = { state: 'unused' }
+        }
       }
 
-      this.autoTranslateByBing((object) => {
-        let newObject = Object.assign({},
-          localeText,
-          object
-        );
-        newObject = this.sortObject(newObject);
-        fs.writeFileSync(
-          `${base}/${to}/${language}.json`,
-          JSON.stringify(newObject, null, 2), 'utf8'
-        );
+      for (let i in newTranslations) {
+        if (!report[i])
+          report[i] = { state: 'new item added' }
+      }
 
-        if (language != sourceLanguage) {
+      this.autoTranslateByBing(
+        object => {
+          let newObject = Object.assign({}, localeText, object)
+          newObject = this.sortObject(newObject)
+          fs.writeFileSync(
+            `${base}/${to}/${language}.json`,
+            JSON.stringify(newObject, null, 2),
+            'utf8'
+          )
 
-          const needTranslations = _.pickBy((v, key) => {
-            return key == (willTransformise ? transformise(v) : v);
-          })(newObject)
+          if (language != sourceLanguage) {
+            const needTranslations = _.pickBy((v, key) => {
+              return (key == (willTransformise ? transformise(v) : v) || v === '')
+            })(newObject)
 
-          let needTranslationsKeys = _.keys(needTranslations)
-
-          if (needTranslationsKeys.length) {
-            console.log(`${needTranslationsKeys.length} item(s) need translations`);
-            console.table(needTranslationsKeys)
+            for (let i in needTranslations) {
+              if (!report[i]) report[i] = { state: 'needs translation' }
+            }
           }
 
-        }
+          if (Object.keys(report).length)
+            console.table(report)
+          else
+            console.log('No issues')
 
-        lock = false;
-      }, newTranslations, language);
-
-    }, 1000);
-
-
+          lock = false
+        },
+        newTranslations,
+        language
+      )
+    }, 1000)
   }
 
   sortObject(obj) {
-    return Object.keys(obj).sort().reduce((result, key) => (
-      Object.assign({}, result, {
-        [key]: obj[key],
-      })
-    ), {});
+    return Object.keys(obj)
+      .sort()
+      .reduce(
+        (result, key) =>
+          Object.assign({}, result, {
+            [key]: obj[key]
+          }),
+        {}
+      )
   }
 
   autoTranslateByBing(callback, object, to) {
-    let count = Object.keys(object).length;
+    let count = Object.keys(object).length
     //源语言则直接返回，不翻译也直接返回
-    if (!this.options.autoTranslate || to === this.options.sourceLanguage || !count) {
-      callback(object);
-      return;
+    if (
+      !this.options.autoTranslate ||
+      to === this.options.sourceLanguage ||
+      !count
+    ) {
+      callback(object)
+      return
     }
     try {
-      let translate = require('./translates/' + this.options.autoTranslate);
-      translate(callback, object, this.options.sourceLanguage, to);
+      let translate = require('./translates/' + this.options.autoTranslate)
+      translate(callback, object, this.options.sourceLanguage, to)
     } catch (e) {
-      callback(object);
-      console.warn(e.message);
+      callback(object)
+      console.warn(e.message)
     }
   }
 }
 
-const argv = require('minimist')(process.argv.slice(2));
-const baseDir = argv.b || argv.baseDirectory || '.';
-const dir = argv.d || argv.directory;
-const functionName = argv.f || argv.functionName || '\\$t';
-const outputDirectory = argv.o || argv.output || 'lang';
-const languages = argv.l || argv.languages || 'en';
-const willTransformise = argv.t || argv.transformise || false;
-const sourceLanguage = argv.s || argv.sourceLanguage || 'zh-CN';
-const autoTranslate = argv.a || argv.autotranslate || false;
+const argv = require('minimist')(process.argv.slice(2))
+const baseDir = argv.b || argv.baseDirectory || '.'
+const dir = argv.d || argv.directory
+const functionName = argv.f || argv.functionName || '\\$t'
+const outputDirectory = argv.o || argv.output || 'lang'
+const languages = argv.l || argv.languages || 'en'
+const willTransformise = argv.t || argv.transformise || false
+const sourceLanguage = argv.s || argv.sourceLanguage || 'zh-CN'
+const autoTranslate = argv.a || argv.autotranslate || false
+const deleteExpired = argv.x || argv.deleteExpired || false
 
 new i18nGenerateJson({
   base: baseDir.replace(/\/$/, ''),
@@ -182,5 +198,6 @@ new i18nGenerateJson({
   autoTranslate: autoTranslate,
   functionName: functionName,
   sourceLanguage: sourceLanguage,
-  willTransformise: willTransformise
-}).run();
+  willTransformise: willTransformise,
+  deleteExpired: deleteExpired
+}).run()
